867f8c37dddadd5ef995d8bda5bec48891c466d5
Arseny Mitin <mitinarseny@gmail.com>
2019-06-16 15:59:05 +0300

    Brew formula update for dots version v1.0.1

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -2,14 +2,14 @@
 class Dots < Formula
   desc "Delivery tool for dotfiles"
   homepage "https://github.com/mitinarseny/dots"
-  version "1.0.0"
+  version "1.0.1"
 
   if OS.mac?
-    url "https://github.com/mitinarseny/dots/releases/download/v1.0.0/dots_1.0.0_Darwin_x86_64.tar.gz"
-    sha256 "0b1ccd4988e4f7ba2a4d23bc96ed65bec5d45c657882bfed841baec7ab726885"
+    url "https://github.com/mitinarseny/dots/releases/download/v1.0.1/dots_1.0.1_Darwin_x86_64.tar.gz"
+    sha256 "bd93b13a2a27ec9207b21399501503c68ffc9992dd07360df4d554d2d8b42d7e"
   elsif OS.linux?
-    url "https://github.com/mitinarseny/dots/releases/download/v1.0.0/dots_1.0.0_Linux_x86_64.tar.gz"
-    sha256 "bbedb2c5f865b20680024e2efbbfe7267aa253c85048a6cf523bb17c331305da"
+    url "https://github.com/mitinarseny/dots/releases/download/v1.0.1/dots_1.0.1_Linux_x86_64.tar.gz"
+    sha256 "b4a9c67d8491b7b755a99f17b9ba2362ba5139cb0a0d03e400c9c68fe128939f"
   end
 
   def install
